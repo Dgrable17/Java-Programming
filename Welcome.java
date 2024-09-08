7b295cf44cdda354bed256bfdc5e663d2f408a74
Dgrable17 <159395640+Dgrable17@users.noreply.github.com>
2024-09-08 18:28:29 -0400

    Add files via upload

---
+++ Welcome.java
@@ -0,0 +1,10 @@
+// This program prints "Welcome to Java" 5 times
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+    }
+}
