3e3a15c9cb346515b18ec4c60208c1ba332c82cf
Dgrable17 <159395640+Dgrable17@users.noreply.github.com>
2024-12-01 17:05:20 -0500

    Update Exercise5_4.java

--- Exercise5_4.java
+++ Exercise5_4.java
@@ -1,15 +1,17 @@
 //This code is to convert miles to kilometers and to display it
 public class Exercise5_4 {
     public static void main(String[] args) {
-        //distinguishing what miles are
+        //declaring miles
         int miles = 1;
+        //declaring KM_PER_MILE
+        double KM_PER_MILE = 1.609;
         
         //prints out this shows the 1-10 conversion of miles kilometers
         System.out.println("This shows the 1-10 conversions of \nMiles Kilometers");
 
         //The while loop that calculates the miles to kilometers and prints said conversions
         while (miles < 11) {
-            double kilometers = (miles * 1.609);
+            double kilometers = (miles * KM_PER_MILE);
             System.out.println (miles + "     " + kilometers);
             miles++;
         }
