8a58e52e45d2cf1fa68c138bdc27ea26df6cb78e
Dgrable17 <159395640+Dgrable17@users.noreply.github.com>
2024-12-01 17:32:21 -0500

    Update Exercise5_7.java

--- Exercise5_7.java
+++ Exercise5_7.java
@@ -1,33 +1,40 @@
-//This is to calculate the tuition of ten years plus the total of four more after the tenth year with a starting tuition of 10,000 that increases by 5% each year 
+import java.util.Scanner;
+//This is to calculate the tuition of ten years plus the total of four more after the tenth year 
 public class Exercise5_7 {
     public static void main(String[] args) {
-        //declaring year and starting tuition and total
+        Scanner input = new Scanner(System.in);
+        //declaring year and total
         int year = 1;
         double total = 0;
-        double tuition = 10000;
-
-        //prints For a university that starts at $10,000 and increases by 5% each year
-        System.out.println("For a university that starts at $10,000 and\n increases by 5% each year.");
-        //prints This will calculate the 10 year and the total of the 4 proceeding years
+        
+        //prints instructions for the user to input
         System.out.println("This will calculate the 10 year and the total of the 4 proceeding years.\n");
+        System.out.println("Please enter the initial tuition you have: ");
+        double initialTuition = input.nextDouble();
+        System.out.println("\nPlease enter the annual increase percentage with a whole number: ");
+        double annualIncreasePercentage = input.nextDouble();
+        input.close();
+
+        //calculates the percentage into an actual percentage
+        double annualIncrease = 1 + (annualIncreasePercentage / 100);
 
         //loop to calculate the first 10 years of tuition
         for (year = 1; year <= 10; year++) {
-            tuition *= 1.05;
+            initialTuition *= annualIncrease;
         }
 
-        //prints Year 10 Tuition is $tuition
-        System.out.printf("Year 10 Tuition is $%.2f\n", tuition);
+        // Prints Year 10 Tuition is $tuition
+        System.out.printf("\nYear 10 Tuition is $%.2f\n", initialTuition);
 
-        //loop to calculate the total using the 4 years after the first 10
+        // Loop to calculate tuition after 10 years with annual increases
         for (year = 11; year <=14; year++) {
-            tuition *= 1.05;
-            total += tuition;
+            initialTuition *= annualIncrease;
+            total += initialTuition;
         }
 
         //prints The total tuition for 11-14 is $total
         System.out.printf("The total tuition for 11-14 is $%.2f\n", total);
         //prints That was the tuition for the 10th year and the total for the 4 years after
-        System.out.println("\nThat was the tuition for the 10th year and the\n total for the 4 years after.");
+        System.out.println("That was the tuition for the 10th year and the\ntotal for the 4 years after.");
     }
 }
